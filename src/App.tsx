19098859a3585164004c1671011b034b46587965
AdilKalbaev04 <adilkalbaev2004@gmail.com>
2026-04-27 10:29:58 +0600

    add icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { I18nextProvider } from "react-i18next";
+import { Helmet } from "react-helmet-async";
 import i18n from "./i18n";
+import photoIcon from "./assets/img/photo.jpg";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import AboutMe from "./components/AboutMe";
@@ -11,23 +13,26 @@ import Footer from "./components/Footer";
 import Technologies from "./components/Technologies";
 
 function App() {
-    return (
-        <I18nextProvider i18n={i18n}>
-            <div className="flex flex-col min-h-screen">
-                <Header />
-                <main>
-                    <Hero />
-                    <AboutMe />
-                    <Services />
-                    <Technologies />
-                    <Projects />
-                    <Reviews />
-                    <Contact />
-                </main>
-                <Footer />
-            </div>
-        </I18nextProvider>
-    );
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Helmet>
+        <link rel="icon" href={photoIcon} type="image/jpeg" />
+      </Helmet>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main>
+          <Hero />
+          <AboutMe />
+          <Services />
+          <Technologies />
+          <Projects />
+          <Reviews />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </I18nextProvider>
+  );
 }
 
 export default App;
